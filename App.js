87ec6aec9219d22f8145a3f6724d5bc813d02256
Rhea3010 <96485811+Rhea3010@users.noreply.github.com>
2025-02-13 13:33:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Canvas from "./components/Canvas.jsx";
+
+function App() {
+    return (
+        <div className="app">
+            <div>
+                <Canvas />
+            </div>
+        </div>
+    );
+}
+
+export default App;
